e539216b43b029334dec0cdd820c13f5e2d81231
Suppachai.m <openpino@hotmail.com>
2018-11-13 15:57:19 +0700

    PaddingOK

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,22 @@ void main() {
 class MyApp extends StatelessWidget {
   //  Explicit
   var titleName = 'Basic Flutter';
+  var messageString = 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of ';
+  var paddingDouble = 35.0;
+  
 
   @override
   Widget build(BuildContext context) {
+
+    Widget widgetTextMessage = new Container(padding: new EdgeInsets.all(paddingDouble), child: new Text(messageString),);
+
     return new MaterialApp(
       title: titleName,
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text(titleName),
         ),
-        body: new Text(titleName),
+        body: widgetTextMessage,
       ),
     );
   }
