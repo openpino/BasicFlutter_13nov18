fd59b1ac028c7bb66d56c3d0e0dde2b455457db9
Suppachai.m <openpino@hotmail.com>
2018-11-14 09:54:27 +0700

    Text Widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,33 @@ void main() {
 class MyApp extends StatelessWidget {
   //  Explicit
   var titleName = 'Basic Flutter';
-  var messageString = 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of ';
+  var messageString =
+      'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of ';
   var paddingDouble = 35.0;
-  
+
+  var friend1String = 'Doramon';
+  var friend2String = 'Nopita';
+  var friend3String = 'Sunako';
 
   @override
   Widget build(BuildContext context) {
 
-    Widget widgetTextMessage = new Container(padding: new EdgeInsets.all(paddingDouble), child: new Text(messageString),);
+    //DEmo Create Text Widget
+    Text friend1Text = new Text(friend1String);
+    Text friend2Text = new Text(friend2String);
+    Text friend3Text = new Text(friend3String);
+
+   // Create widget for body
+    Widget widgetTextMessage = new Container(
+      padding: new EdgeInsets.all(paddingDouble),
+      child: new Text(messageString),
+    );
+
+    Widget widgetMyFriend = new Container(
+      child: new Row(
+        children: <Widget>[friend1Text, friend2Text, friend3Text],
+      ),
+    );
 
     return new MaterialApp(
       title: titleName,
@@ -22,7 +41,7 @@ class MyApp extends StatelessWidget {
         appBar: new AppBar(
           title: new Text(titleName),
         ),
-        body: widgetTextMessage,
+        body: widgetMyFriend,
       ),
     );
   }
